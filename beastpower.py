3be7427ebc92d17ae5f38edd2879acbb40ba5387
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2023-09-01 12:51:02 +1200

    Update beastpower.py
    added pierce consumpiton

--- beastpower.py
+++ beastpower.py
@@ -14,11 +14,20 @@ def grid(minimum, maximum):
     d_m = 2
     d_c = d_m
 
+    #Ceramics, MOABs, BFBs
+    ceramic = 0
+    moab = 0
+    bfb = 0
+
     for i in range(minimum, maximum + 1):
         b_c = i
         p_c = p_m + (p_M - p_m)/(b_M - b_m) * (b_c - b_m)
         d_c = d_m + (d_M - d_m)/(b_M - b_m) * (b_c - b_m)
+        ceramic = roof(p_c/2)
+        moab = roof(p_c/10)
+        bfb = roof(p_c/30)
+        
         print("|-")
-        print("| %s || %s || %s || ??? || Picks up to ??? Ceramics, ??? MOABs, ??? BFBs, and 1 (targetable) DDT" % (i, p_c, d_c))
+        print("| %s || %s || %s || ??? || Picks up to %s Ceramics, %s MOABs, %s BFBs, and 2 (targetable) DDT" % (i, p_c, d_c, ceramic, moab, bfb))
 
-grid(16, 32)
+grid(33, 63)

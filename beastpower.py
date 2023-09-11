030da2f571face945c203e979304c37b3075b0f9
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2023-09-11 15:47:24 +1200

    Update beastpower.py
    added pouakai

--- beastpower.py
+++ beastpower.py
@@ -19,7 +19,7 @@ def grid_003(minimum, maximum):
     d_c = d_m #Current damage
     regrow_d_bonus = 1 #Regrow damage bonus
 
-    #Ceramics, MOABs, BFBs
+    #Pierce consumption and total "pierce" of heavier bloons: Ceramics, MOABs
     ceramic = 0 #Quantity of Ceramics picked up
     moab = 0 #Quantity of MOABs picked up
 
@@ -61,7 +61,7 @@ def grid_004(minimum, maximum):
     moab_d_bonus = 11 #MOAB-class damage bonus
     regrow_d_bonus = 1 #Regrow damage bonus
 
-    #Ceramics, MOABs, BFBs
+    #Pierce consumption and total "pierce" of heavier bloons: Ceramics, MOABs, BFBs, targetable DDTs
     ceramic = 0 #Quantity of Ceramics picked up
     moab = 0 #Quantity of MOABs picked up
     bfb = 0 #Quantity of BFBs picked up
@@ -86,6 +86,49 @@ def grid_004(minimum, maximum):
     print("-------------------------------------------------")
     print("")
 
+# GIANT CONDOR GRID
+def grid_005(minimum, maximum):
+    print("Pouakai...")
+    print("")
+    
+    # Beast Power
+    b_M = 132 # Max beast power
+    b_m = 36 # Min beast power 
+    b_c = b_m #Current beast power
+    # Pierce
+    p_M = 450 #Max pierce
+    p_m = 200 #Min pierce
+    p_c = p_m #Current pierce
+    # Damage
+    d_M = 60 #Max damage
+    d_m = 20 #Min damage
+    d_c = d_m #Current damage
+    moab_d_bonus = 110 #MOAB-class damage bonus
+    regrow_d_bonus = 1 #Regrow damage bonus
+
+    #Pierce consumption and total "pierce" of heavier bloons: MOABs, BFBs, DDTs, ZOMGs
+    moab = 0 #Quantity of MOABs picked up
+    bfb = 0 #Quantity of BFBs picked up
+    ddt = 0 #Quantity of DDTs picked up
+    zomg = 0 #Quantity of ZOMGs picked up
+
+    # NOTE TO SELF: Perhaps it is better to not hardcode pierce consumptions into heavy bloon types; it should be a better idea to just make a ceramic_consumption etc. type variables and then softcode them from there...
+    for i in range(minimum, maximum + 1):
+        b_c = i
+        p_c = math.floor(p_m + (p_M - p_m)/(b_M - b_m) * (b_c - b_m))
+        d_c = math.floor(d_m + (d_M - d_m)/(b_M - b_m) * (b_c - b_m))
+        moab = math.ceil(p_c/10) #calculate total max picks from pierce consumption of MOABs
+        bfb = math.ceil(p_c/30) #calculate total max picks from pierce consumption of BFBs
+        ddt = math.ceil(p_c/30) #calculate total max picks from pierce consumption of DDTs
+        zomg = math.ceil(p_c/50) #calculate total max picks from pierce consumption of ZOMGs
+        cooldown = round(0.6-(b_c-b_m)/(b_M-b_m) * 0.286796242, 4) # Rounds to 4dp
+        
+        print("|-")
+        print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s MOABs, %s BFBs, %s ZOMGs, and %s DDTs" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, moab, bfb, zomg, ddt))
+    print("-------------------------------------------------")
+    print("")
+
 # PRINTING GRIDS
 grid_003(8, 24)
 grid_004(16, 64)
+grid_005(36, 132)

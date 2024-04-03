b17de0cb1ea2f36ab1877af278225b53a5902385
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2024-04-03 20:16:29 +1300

    Update beastpower.py
    shark update?

--- beastpower.py
+++ beastpower.py
@@ -25,11 +25,11 @@ def grid_300(minimum, maximum):
         b_c = i #Increment the current beast power along the rows
         p_c = math.floor(p_m + (p_M - p_m)/(b_M - b_m) * (b_c - b_m)) # Calculate pierce
         d_c = math.floor(d_m + (d_M - d_m)/(b_M - b_m) * (b_c - b_m)) # Calculate damage
-        cooldown = round(0.55-(b_c-b_m)/(b_M-b_m) * (0.55 - 0.36), 4) # Rounds to 4dp
+        cooldown_splash = round(0.55-(b_c-b_m)/(b_M-b_m) * (0.55 - 0.36), 4) # Rounds to 4dp
         k_c = round(k_m + (k_M - k_m)/(b_M - b_m) * (b_c - b_m), 4) # Calculate knockback, rounds to 4dp
         
         print("|-")
-        print("| %s || 1 (grab)<br />%s (splash) || 250,000 (grab, instakill trigger)<br />%s (splash)|| ???s (grapple)<br />%ss (splash, ???x) || Inflicts %ss knockback via splash." % (i, p_c, d_c, cooldown, k_c))
+        print("| %s || 1 (grab)<br />%s (splash) || 250,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash) || Inflicts %ss knockback via splash." % (i, p_c, d_c, cooldown_splash, k_c))
 
     print("-------------------------------------------------")
     print("")
@@ -425,7 +425,7 @@ def grid_005(minimum, maximum):
     print("")
 
 # PRINTING GRIDS
-#grid_300(8, 24)
+grid_300(8, 24)
 #grid_020(3, 6)
 #grid_030(8, 24)
 #grid_040(16, 64)
@@ -433,4 +433,4 @@ def grid_005(minimum, maximum):
 #grid_002(3, 6)
 #grid_003(8, 24)
 #grid_004(16, 64)
-grid_005(36, 132)
+#grid_005(36, 132)

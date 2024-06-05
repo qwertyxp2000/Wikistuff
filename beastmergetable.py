a60cea54a02dffc8122aa812015897a6a5d3cf48
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2024-06-05 20:30:34 +1200

    Update beastmergetable.py
    solution now works as intended

--- beastmergetable.py
+++ beastmergetable.py
@@ -21,43 +21,42 @@ def RoundToFive(num): #the annoying BTD6 rounding stuff
         rem = rem - 5
     return round(num-rem)
 
-def easyify(cost): #purpose is to make the Easy cost and do the annoying rounding junk afterwards
-    cost = RoundToFive(cost * 0.85)
-    return cost
-def hardify(cost): #purpose is to make the Easy cost and do the annoying rounding junk afterwards
-    cost = RoundToFive(cost * 1.08)
-    return cost
-def impoppableify(cost): #purpose is to make the Easy cost and do the annoying rounding junk afterwards
-    cost = RoundToFive(cost * 1.2)
-    return cost
-
-#Whole Costs Table
+#Upgrade Costs Table
 beastupgrade_cost_table = []
-for i in range(0, 4): #Easy, Medium, Hard, Impoppable
-    beastupgrade_cost_table.append([ [], [], [] ]) #Start with appending an empty three-unit list, the three units for the Path 1-3 upgrades
+for i in range(0, len(difficulty_multiplier)): #Start with appending an empty four-unit list, the four units for Easy, Medium, Hard, Impoppable
+    beastupgrade_cost_table.append([])
+    for j in range(0, len(beastupgrade_cost_medium)): #Append the empty three-unit list, the three units for the Path 1-3 upgrades
+        beastupgrade_cost_table[i].append([])
+#print(beastupgrade_cost_table)
+# i = Difficulty (Easy, Medium, Hard, Impoppable)
+# j = The Beast Handler upgrade path (0, 1, 2; top, middle, bottom)
+# k = The Beast Handler upgrade tier of a upgrade path (0-3, or in this case Tiers 1-4)
+for i in range(0, len(difficulty_multiplier)): #Easy, Medium, Hard, Impoppable
     for j in range(0, len(beastupgrade_cost_medium)):
-        for k in range(0, 4):
+        for k in range(0, len(beastupgrade_cost_medium[j])):
             beastupgrade_cost_table[i][j].append(RoundToFive(beastupgrade_cost_medium[j][k] * difficulty_multiplier[i]))
-print(beastupgrade_cost_table)
+#print(beastupgrade_cost_table)
 
-#DEPRECATED
-#-------------------------------------------------------------
-#Easy Costs Table
-beastupgrade_cost_easy = [ [], [], [] ]
-for i in range(0, len(beastupgrade_cost_easy)):
-    for j in range(0, 4):
-               beastupgrade_cost_easy[i].append(easyify(beastupgrade_cost_medium[i][j]))
-#Hard Costs Table (n.b. there's gotta be an easier way to write this junk)
-beastupgrade_cost_hard = [ [], [], [] ]
-for i in range(0, len(beastupgrade_cost_hard)):
-    for j in range(0, 4):
-               beastupgrade_cost_hard[i].append(hardify(beastupgrade_cost_medium[i][j]))
-#Impoppable Costs Table (n.b. there's gotta be an easier way to write this junk)
-beastupgrade_cost_impoppable = [ [], [], [] ]
-for i in range(0, len(beastupgrade_cost_impoppable)):
-    for j in range(0, 4):
-               beastupgrade_cost_impoppable[i].append(impoppableify(beastupgrade_cost_medium[i][j]))
-#-------------------------------------------------------------
+# Cumulative Costs Table
+beastcumulative_cost_table = []
+for i in range(0, len(difficulty_multiplier)): #Start with appending an empty four-unit list, the four units for Easy, Medium, Hard, Impoppable
+    beastcumulative_cost_table.append([])
+    for j in range(0, len(beastupgrade_cost_medium)): #Append the empty three-unit list, the three units for the Path 1-3 upgrades
+        beastcumulative_cost_table[i].append([])
+#print(beastcumulative_cost_table)
+# i = Difficulty (Easy, Medium, Hard, Impoppable)
+# j = The Beast Handler upgrade path (0, 1, 2; top, middle, bottom)
+# k = The Beast Handler upgrade tier of a upgrade path (0-3, or in this case Tiers 1-4)
+# l = Summation process from 0 to k for that Beast Handler summation process
+for i in range(0, len(difficulty_multiplier)): #Easy, Medium, Hard, Impoppable
+    for j in range(0, len(beastupgrade_cost_medium)):
+        for k in range(0, len(beastupgrade_cost_medium[j])):
+            beast_summation = beasthandler_cost
+            for l in range(0, k + 1): #Increased cap of max by 1 so that the process is shifted 1 up; basically from 1 to (k + 1) instead of 0 to k. If 0 to k, then we get the default "0" which would be just the Beast Handler base cost without any of that extra calculations inside that condition block
+                beast_summation += beastupgrade_cost_table[i][j][l] #go from 0-k again so the summation actually can occur
+            #print(beast_summation)
+            beastcumulative_cost_table[i][j].append(beast_summation)
+#print(beastcumulative_cost_table)
 
 ##Testing that the tables are working as expected
 #print(beastupgrade_cost_medium)
@@ -69,18 +68,19 @@ for i in range(0, len(beastupgrade_cost_impoppable)):
 #print("-----------------------------------------------------------------")
 
 #Now to write the actual tables
-print("|-")
-for i in range(0, (len(beastupgrade_cost_medium))):
-    for j in range(0, (len(beastupgrade_cost_medium[i]))):
-        #these (i==0)*mathematics stuff are bool conditions, sorta like a compressed IF statement, also a non-newline ending with just a space
-        print("|- %s-%s-%s" % ((i == 0) * (j + 1), (i == 1) * (j + 1), (i == 2) * (j + 1)), end = " ")
+# j = The Beast Handler upgrade path (0, 1, 2; top, middle, bottom)
+# k = The Beast Handler upgrade tier of a upgrade path (0-3, or in this case Tiers 1-4)
+# l = Difficulty but it is a pushing process
+for j in range(0, len(beastcumulative_cost_table[i])):
+    for k in range(0, len(beastcumulative_cost_table[i][j])):
+        print("|-")
+        #these (j==0)*mathematics stuff are bool conditions, sorta like a compressed IF statement, also a non-newline ending with just a space
+        print("| %s-%s-%s" % ((j == 0) * (k + 1), (j == 1) * (k + 1), (j == 2) * (k + 1)), end = " ")
         #Hint: int() on a boolean results in 0 or 1 depending on false or true
-
-        beast_summation = beasthandler_cost
-        for k in range(0, j + 1):
-            beast_summation += beastupgrade_cost_medium[i][k] 
-        print("||", end = " ")
-        print("$%s, %s power" % (beast_summation, beastpowermax[j]), end = " ")
-        print("<br />", end = " ")
-        print("$%s / %s power" % (round(beast_summation / beastpowermax[j], 4), beastpowermax[j]), end = " ")
+        for l in range(0, len(difficulty_multiplier)): #Easy, Medium, Hard, Impoppable
+            #print(beastcumulative_cost_table[l][j][k])
+            print("||", end = " ")
+            print("$%s, %s power" % (beastcumulative_cost_table[l][j][k], beastpowermax[k]), end = " ")
+            print("<br />", end = " ")
+            print("$%s / %s power" % (round(beastcumulative_cost_table[l][j][k] / beastpowermax[k], 4), beastpowermax[k]), end = " ")
         print("")

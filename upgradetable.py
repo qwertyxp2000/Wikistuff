3e4fcea269ea52c8f99f2d7cd64e95a1f13d830c
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2024-06-24 22:05:49 +1200

    Create upgradetable.py

---
+++ upgradetable.py
@@ -0,0 +1,15 @@
+import math
+
+#Base cost of the tower
+basetower = 200
+
+#The table of prices of the upgrades of that tower
+upgradetable = [
+  [140, 220, 300, 1800, 15000],
+  [100, 190, 400, 7500, 45000],
+  [90, 200, 625, 2000, 21500]
+]
+
+#Difficulty multiplier for Easy, Medium, Hard, and Impoppable
+difficulty_multiplier = [0.85, 1, 1.08, 1.2]
+

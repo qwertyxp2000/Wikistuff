a6f30bd1320c771676848f367f38f226e5adbe17
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2024-08-01 13:47:00 +1200

    Update beastpower.py
    condor and pouakai, assuming pouakai no ceramic penalty

--- beastpower.py
+++ beastpower.py
@@ -460,18 +460,19 @@ def grid_004(minimum, maximum):
         b_c = i
         p_c = math.floor(p_m + (p_M - p_m)/(b_M - b_m) * (b_c - b_m))
         d_c = math.floor(d_m + (d_M - d_m)/(b_M - b_m) * (b_c - b_m))
+        ceramic = math.ceil(p_c/2) #calculate total max picks from pierce consumption of Ceramics
         moab = math.ceil(p_c/15) #calculate total max picks from pierce consumption of MOABs
         bfb = math.ceil(p_c/30) #calculate total max picks from pierce consumption of BFBs
-        ddt = math.ceil(p_c/60) #calculate total max picks from pierce consumption of targetable DDTs
+        ddt = math.ceil(p_c/30) #calculate total max picks from pierce consumption of targetable DDTs
         cooldown = round(0.6-(b_c-b_m)/(b_M-b_m) * 0.28, 4) # Rounds to 4dp
         
         print("|-")
         if (b_c == b_M): #the 64/64 case
-            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s MOABs, %s BFBs, and %s (targetable) DDTs. Can target and damage any unpickable MOAB-class bloon." % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, moab, bfb, ddt))
-        elif (ddt > 1): #the bigger DDT pickups case, which here is 33/64
-            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s MOABs, %s BFBs, and %s (targetable) DDTs" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, moab, bfb, ddt))
+            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDTs. Can target and damage any unpickable MOAB-class bloon." % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt))
+        elif (ddt > 1): #the bigger DDT pickups case
+            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDTs" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt))
         else:
-            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s MOABs, %s BFBs, and %s (targetable) DDT" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, moab, bfb, ddt))
+            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDT" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt))
     print("-------------------------------------------------")
     print("")
 
@@ -508,7 +509,7 @@ def grid_005(minimum, maximum):
         d_c = math.floor(d_m + (d_M - d_m)/(b_M - b_m) * (b_c - b_m))
         moab = math.ceil(p_c/15) #calculate total max picks from pierce consumption of MOABs
         bfb = math.ceil(p_c/30) #calculate total max picks from pierce consumption of BFBs
-        ddt = math.ceil(p_c/60) #calculate total max picks from pierce consumption of DDTs
+        ddt = math.ceil(p_c/30) #calculate total max picks from pierce consumption of DDTs
         zomg = math.ceil(p_c/60) #calculate total max picks from pierce consumption of ZOMGs
         cooldown = round(0.6-(b_c-b_m)/(b_M-b_m) * 0.28, 4) # Rounds to 4dp
         
@@ -518,14 +519,14 @@ def grid_005(minimum, maximum):
     print("")
 
 # PRINTING GRIDS
-grid_300(8, 24)
-grid_400(16, 64)
-grid_500(36, 132)
+#grid_300(8, 24)
+#grid_400(16, 64)
+#grid_500(36, 132)
 #grid_020(3, 6)
 #grid_030(8, 24)
 #grid_040(16, 64)
 #grid_050(36, 132)
 #grid_002(3, 6)
 #grid_003(8, 24)
-#grid_004(16, 64)
-#grid_005(36, 132)
+grid_004(16, 64)
+grid_005(36, 132)

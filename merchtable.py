198221b0934a97d6fb0c919b3825fdf1ee63c451
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2024-08-31 16:59:39 +1200

    Create merchtable.py

---
+++ merchtable.py
@@ -0,0 +1,16 @@
+# |-
+# | [[file:Placeholder other.png|150px]]<br />Dart Monkey Vinyl Figure || $??.??? || [insertDate] || ??? sold, ???% funded || [insertDate] || The link if applicable
+
+filename = "DartMonkey 360 1 1000x.png.png"
+campaign = "https://www.makeship.com/products/dart-monkey-vinyl-figure?utm_source=&utm_medium=&utm_campaign=Q4_2022_NinjaKiwi&utm_content=Gaming_NK"
+name = "Dart Monkey Vinyl Figure"
+cost = 29.99
+date = "21st October 2022 to 19th November 2022"
+sold = 1997
+funded = 399
+prodandman = "Production started on 19th November 2022, shipped on March 10th 2023"
+link = "[[Dart Monkey (BTD6)#Merchandise|Dart Monkey (BTD6) (Merchandise)]]"
+
+
+print("|-")
+print("| [[File:%s|150px]]<br />[%s %s] || $%s || %s || %s sold, %s%% funded || %s || %s" % (filename, campaign, name, cost, date, sold, funded, prodandman, link))

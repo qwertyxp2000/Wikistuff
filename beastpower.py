ad4c5d28c6f47b61350d3a0c2d440c6fb6d2eb90
qwertyxp2000 <60592020+qwertyxp2000@users.noreply.github.com>
2024-11-21 00:58:30 +1300

    Update beastpower.py
    owl to pouakai speed thing

--- beastpower.py
+++ beastpower.py
@@ -36,9 +36,9 @@ def grid_300(minimum, maximum):
         
         print("|-")
         if (b_c == b_M):
-            print("| %s || 1 (grab)<br />%s (splash) || 250,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash. Can trigger instakill on MOABs with less than threshold amount, pulling a single target into the water." % (i, p_c, d_c, att_c, percentage_attack, k_c))
+            print("| %s || 1 (grab)<br />%s (splash) || 500,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash. Can trigger instakill on MOABs with less than threshold amount, pulling a single target into the water." % (i, p_c, d_c, att_c, percentage_attack, k_c))
         else:
-            print("| %s || 1 (grab)<br />%s (splash) || 250,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash." % (i, p_c, d_c, att_c, percentage_attack, k_c))
+            print("| %s || 1 (grab)<br />%s (splash) || 500,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash." % (i, p_c, d_c, att_c, percentage_attack, k_c))
 
     print("-------------------------------------------------")
     print("")
@@ -80,9 +80,9 @@ def grid_400(minimum, maximum):
         
         print("|-")
         if (b_c == b_M):
-            print("| %s || 1 (grab)<br />%s (splash) || 250,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash. Can trigger instakill on MOABs, BFBs, ZOMGs, and visible DDTs with less than threshold amount, pulling a single target into the water." % (i, p_c, d_c, att_c, percentage_attack, k_c))
+            print("| %s || 1 (grab)<br />%s (splash) || 500,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash. Can trigger instakill on MOABs, BFBs, ZOMGs, and visible DDTs with less than threshold amount, pulling a single target into the water." % (i, p_c, d_c, att_c, percentage_attack, k_c))
         else:
-            print("| %s || 1 (grab)<br />%s (splash) || 250,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash. Can trigger instakill on MOABs and BFBs with less than threshold amount, pulling a single target into the water." % (i, p_c, d_c, att_c, percentage_attack, k_c))
+            print("| %s || 1 (grab)<br />%s (splash) || 500,000 (grab, instakill trigger)<br />%s (splash)|| 1.1s (grapple)<br />%ss (splash, %sx) || Inflicts %ss knockback via splash. Can trigger instakill on MOABs and BFBs with less than threshold amount, pulling a single target into the water." % (i, p_c, d_c, att_c, percentage_attack, k_c))
 
     print("-------------------------------------------------")
     print("")
@@ -369,6 +369,10 @@ def grid_002(minimum, maximum):
     d_m = 1 #Min damage
     d_c = d_m #Current damage
     regrow_d_bonus = 1 #Regrow damage bonus
+    # Flight speed
+    f_M = 120
+    f_m = 80
+    f_c = f_m
 
     #Pierce consumption and total "pierce" of heavier bloons: Ceramics, MOABs
     lead = 0 #Quantity of Leads picked up
@@ -380,11 +384,12 @@ def grid_002(minimum, maximum):
         p_c = math.floor(p_m + (p_M - p_m)/(b_M - b_m) * (b_c - b_m)) # Calculate pierce
         d_c = math.floor(d_m + (d_M - d_m)/(b_M - b_m) * (b_c - b_m)) # Calculate damage
         lead = math.floor(p_c/2) #calculate total max picks from pierce consumption of Leads; NOTE: pierce consumption on Leads floors, not roofs
-        ceramic = math.floor(p_c/6) #calculate total max picks from pierce consumption of Ceramics; NOTE: pierce consumption on Ceramics floors, not roofs
-        cooldown = round(0.7-(b_c-b_m)/(b_M-b_m) * 0.28, 4) # Rounds to 4dp
+        ceramic = math.floor(p_c/5) #calculate total max picks from pierce consumption of Ceramics; NOTE: pierce consumption on Ceramics floors, not roofs
+        cooldown = round(1-(b_c-b_m)/(b_M-b_m) * 0.4, 4) # Rounds to 4dp
+        flight = round(f_m + (f_M - f_m) * (b_c - b_m)/(b_M - b_m), 4) # Rounds to 4dp
         
         print("|-")
-        print("| %s || %s || %s (+%s Regrow) || %ss || Picks up to %s Leads, %s Ceramics" % (i, p_c, d_c, regrow_d_bonus, cooldown, lead, ceramic))
+        print("| %s || %s || %s (+%s Regrow) || %ss || Picks up to %s Leads, %s Ceramics. Flight speed of %s." % (i, p_c, d_c, regrow_d_bonus, cooldown, lead, ceramic, flight))
 
     print("-------------------------------------------------")
     print("")
@@ -407,6 +412,10 @@ def grid_003(minimum, maximum):
     d_m = 1 #Min damage
     d_c = d_m #Current damage
     regrow_d_bonus = 1 #Regrow damage bonus
+    # Flight speed
+    f_M = 120
+    f_m = 80
+    f_c = f_m
 
     #Pierce consumption and total "pierce" of heavier bloons: Ceramics, MOABs
     ceramic = 0 #Quantity of Ceramics picked up
@@ -419,13 +428,14 @@ def grid_003(minimum, maximum):
         d_c = math.floor(d_m + (d_M - d_m)/(b_M - b_m) * (b_c - b_m)) # Calculate damage
         ceramic = math.ceil(p_c/2) #calculate total max picks from pierce consumption of Ceramics
         moab = math.ceil(p_c/1089) #calculate total max picks from pierce consumption of MOABs
-        cooldown = round(0.6-(b_c-b_m)/(b_M-b_m) * 0.28, 4) # Rounds to 4dp
+        cooldown = round(1-(b_c-b_m)/(b_M-b_m) * 0.48, 4) # Rounds to 4dp
+        flight = round(f_m + (f_M - f_m) * (b_c - b_m)/(b_M - b_m), 4) # Rounds to 4dp
         
         print("|-")
         if (b_c == b_M):
-            print("| %s || %s || %s (+%s Regrow) || %ss || Can pick up MOABs. Picks up to %s Ceramics, %s MOABs" % (i, p_c, d_c, regrow_d_bonus, cooldown, ceramic, moab))
+            print("| %s || %s || %s (+%s Regrow) || %ss || Can pick up MOABs. Picks up to %s Ceramics, %s MOABs. Flight speed of %s." % (i, p_c, d_c, regrow_d_bonus, cooldown, ceramic, moab, flight))
         else:
-            print("| %s || %s || %s (+%s Regrow) || %ss || Picks up to %s Ceramics" % (i, p_c, d_c, regrow_d_bonus, cooldown, ceramic))
+            print("| %s || %s || %s (+%s Regrow) || %ss || Picks up to %s Ceramics. Flight speed of %s." % (i, p_c, d_c, regrow_d_bonus, cooldown, ceramic, flight))
 
     print("-------------------------------------------------")
     print("")
@@ -449,6 +459,10 @@ def grid_004(minimum, maximum):
     d_c = d_m #Current damage
     moab_d_bonus = 11 #MOAB-class damage bonus
     regrow_d_bonus = 1 #Regrow damage bonus
+    # Flight speed
+    f_M = 120
+    f_m = 80
+    f_c = f_m
 
     #Pierce consumption and total "pierce" of heavier bloons: Ceramics, MOABs, BFBs, targetable DDTs
     moab = 0 #Quantity of MOABs picked up
@@ -464,15 +478,16 @@ def grid_004(minimum, maximum):
         moab = math.ceil(p_c/15) #calculate total max picks from pierce consumption of MOABs
         bfb = math.ceil(p_c/30) #calculate total max picks from pierce consumption of BFBs
         ddt = math.ceil(p_c/30) #calculate total max picks from pierce consumption of targetable DDTs
-        cooldown = round(0.6-(b_c-b_m)/(b_M-b_m) * 0.28, 4) # Rounds to 4dp
+        cooldown = round(1-(b_c-b_m)/(b_M-b_m) * 0.48, 4) # Rounds to 4dp
+        flight = round(f_m + (f_M - f_m) * (b_c - b_m)/(b_M - b_m), 4) # Rounds to 4dp
         
         print("|-")
         if (b_c == b_M): #the 64/64 case
-            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDTs. Can target and damage any unpickable MOAB-class bloon." % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt))
+            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDTs. Can target and damage any unpickable MOAB-class bloon. Flight speed of %s." % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt, flight))
         elif (ddt > 1): #the bigger DDT pickups case
-            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDTs" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt))
+            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDTs. Flight speed of %s." % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt, flight))
         else:
-            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDT" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt))
+            print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s Ceramics, %s MOABs, %s BFBs, and %s (targetable) DDT. Flight speed of %s." % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, ceramic, moab, bfb, ddt, flight))
     print("-------------------------------------------------")
     print("")
 
@@ -495,6 +510,10 @@ def grid_005(minimum, maximum):
     d_c = d_m #Current damage
     moab_d_bonus = 80 #MOAB-class damage bonus
     regrow_d_bonus = 1 #Regrow damage bonus
+    # Flight speed
+    f_M = 120
+    f_m = 80
+    f_c = f_m
 
     #Pierce consumption and total "pierce" of heavier bloons: MOABs, BFBs, DDTs, ZOMGs
     moab = 0 #Quantity of MOABs picked up
@@ -512,9 +531,10 @@ def grid_005(minimum, maximum):
         ddt = math.ceil(p_c/30) #calculate total max picks from pierce consumption of DDTs
         zomg = math.ceil(p_c/60) #calculate total max picks from pierce consumption of ZOMGs
         cooldown = round(0.8-(b_c-b_m)/(b_M-b_m) * 0.52, 4) # Rounds to 4dp
+        flight = round(f_m + (f_M - f_m) * (b_c - b_m)/(b_M - b_m), 4) # Rounds to 4dp
         
         print("|-")
-        print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s MOABs, %s BFBs, %s ZOMGs, and %s DDTs" % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, moab, bfb, zomg, ddt))
+        print("| %s || %s || %s (+%s MOAB-class, +%s Regrow) || %ss || Picks up to %s MOABs, %s BFBs, %s ZOMGs, and %s DDTs. Flight speed of %s." % (i, p_c, d_c, moab_d_bonus, regrow_d_bonus, cooldown, moab, bfb, zomg, ddt, flight))
     print("-------------------------------------------------")
     print("")
 
@@ -526,7 +546,7 @@ def grid_005(minimum, maximum):
 #grid_030(8, 24)
 #grid_040(16, 64)
 #grid_050(36, 132)
-#grid_002(3, 6)
-#grid_003(8, 24)
-#grid_004(16, 64)
+grid_002(3, 6)
+grid_003(8, 24)
+grid_004(16, 64)
 grid_005(36, 132)
